3c14cdb2a90c9d3ba0920b1daafa72c664cc23bf
naitikjain3071 <70900997+naitikjain3071@users.noreply.github.com>
2021-12-01 20:04:59 +0530

    updated Quiz_Play to scroll the page

--- quiz-app-django-python/Quiz_Play.py
+++ quiz-app-django-python/Quiz_Play.py
@@ -1,6 +1,7 @@
 from logging import error
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+import time
 
 username = "n123"
 password = "n123"
@@ -17,9 +18,10 @@ driver.find_element_by_id("start_attempt_quiz").click()
 driver.find_element_by_id("IPL 2021 Phase 2 started on?-September 19").click()
 driver.find_element_by_id("In the IPL 2015, which bowler won the Purple Cap?-Bhuvneshwar Kumar").click()
 driver.find_element_by_id("Who won IPL in 2021-Chennai Super Kings").click()
-driver.find_element_by_id("Who was the first batsman to score a century in the IPL?-Virendra Sehwag").click()
-driver.find_element_by_id("Who among the following teams did not reach the semi finals of IPL 2008?-Delhi Daredevils").click()
-driver.find_element_by_id("How many English bowlers have taken a five-wicket haul in the IPL?-1").click()
+driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+driver.find_element_by_id("Who was the first batsman to score a century in the IPL?-Virendra Sehwag")
+driver.find_element_by_id("Who among the following teams did not reach the semi finals of IPL 2008?-Delhi Daredevils")
+driver.find_element_by_id("How many English bowlers have taken a five-wicket haul in the IPL?-1")
 driver.find_element_by_id("Which of these IPL franchise was banned for two years?-Chennai Super Kings").click()
 driver.find_element_by_id("Which among these cities has hosted the most number of IPL matches?-Bangalore").click()
 driver.find_element_by_id("Which season(s) had the most number of hat-tricks?-2014").click()
